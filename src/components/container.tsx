6f92fb636a912ff69200d8f8e9532fdac29c564d
Ashaduzzaman Rifat <rftzmn1@gmail.com>
2025-07-04 21:56:00 +0600

    routing added & Homepage updated

---
+++ src/components/container.tsx
@@ -0,0 +1,10 @@
+type ContainerProps = {
+  children: React.ReactNode;
+};
+export default function Container({ children }: ContainerProps) {
+  return (
+    <div className='flex flex-col max-w-7xl mx-auto bg-white/[2%] min-h-screen'>
+      {children}
+    </div>
+  );
+}
