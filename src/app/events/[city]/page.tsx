5333a085e8b8f28009ccf9beb2d433323bab3b08
Ashaduzzaman Rifat <rftzmn1@gmail.com>
2025-07-08 20:09:04 +0600

    data fetched & search complete by city

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,3 +1,30 @@
-export default function EventsPage() {
-  return <div>Events Page</div>;
+import EventsList from "@/components/event-list";
+import H1 from "@/components/h1";
+import { EventoEvent } from "@/lib/types";
+
+type EventsPageProps = {
+  params: {
+    city: string;
+  };
+};
+
+export default async function EventsPage({ params }: EventsPageProps) {
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events?city=austin`
+  );
+  const events: EventoEvent[] = await response.json();
+
+  const city = params.city;
+
+  return (
+    <main className='flex flex-col items-center py-24 px-[20px] min-h-[110vh]'>
+      <H1 className='mb-28'>
+        {city === "all" && "All Events"}
+        {city !== "all" &&
+          `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
+      </H1>
+      <EventsList events={events} />
+    </main>
+  );
 }
+``;
