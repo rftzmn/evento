fb8e8cdfca1a78d4c5c7cd2762182f2354bfc2dc
Ashaduzzaman Rifat <rftzmn1@gmail.com>
2025-07-08 21:39:57 +0600

    disabled linting

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,12 @@ const nextConfig = {
             { protocol: 'https', hostname: 'bytegrad.com' },
             { protocol: 'https', hostname: 'images.unsplash.com' }
         ]
-    }
+    },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
